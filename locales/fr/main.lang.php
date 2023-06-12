8f3fe22ca42ad7c4bc302cfc620fe0fccd50b736
franck <carnet.franck.paul@gmail.com>
2023-06-12 09:00:43 +0200

    l10n

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -11,20 +11,20 @@
 
 use Dotclear\Helper\L10n;
 
-L10n::$locales['Authors'] = 'Auteurs';
-L10n::$locales['List of authors'] = 'Liste des auteurs';
-L10n::$locales['This author\'s entries Atom feed'] = 'Fil Atom des billets de cet auteur';
-L10n::$locales['This authors\'s entries comments Atom feed'] = 'Fil Atom des commentaires de cet auteur';
-L10n::$locales['Author Mode'] = 'authorMode';
+L10n::$locales['Authors'] = 'Auteur·rice';
+L10n::$locales['List of authors'] = 'Liste des auteur·rice';
+L10n::$locales['This author\'s entries Atom feed'] = 'Fil Atom des billets de cet·te auteur·rice';
+L10n::$locales['This authors\'s entries comments Atom feed'] = 'Fil Atom des commentaires de cet·te auteur·rice';
+L10n::$locales['Author Mode'] = 'Mode auteur·rice·s';
 L10n::$locales['authorMode'] = 'authorMode';
-L10n::$locales['Author\'s description:'] = 'Description de l\'auteur :';
-L10n::$locales['Author\'s page'] = 'Page de l\'auteur';
+L10n::$locales['Author\'s description:'] = 'Description de l\'auteur·rice :';
+L10n::$locales['Author\'s page'] = 'Page de l\'auteur·rice';
 L10n::$locales['Enable authorMode'] = 'Activer authorMode';
 L10n::$locales['Advanced options'] = 'Options avancées';
 L10n::$locales['URLs prefixes'] = 'Préfixes des URLs';
-L10n::$locales['URL author:'] = 'URL d\'un auteur :';
-L10n::$locales['URL authors:'] = 'URL de la liste des auteurs :';
+L10n::$locales['URL author:'] = 'URL d\'un·e auteur·rice :';
+L10n::$locales['URL authors:'] = 'URL de la liste des auteur·rice·s :';
 L10n::$locales['List options'] = 'Options de liste';
-L10n::$locales['List only authors of standard posts'] = 'Lister seulement les auteurs de billets standards';
+L10n::$locales['List only authors of standard posts'] = 'Lister seulement les auteur·rice·s de billets standards';
 L10n::$locales['Sort list (alphabetical order)'] = 'Trier la liste (ordre alphabétique)';
-L10n::$locales['AuthorMode: authors'] = 'AuthorMode : auteurs';
+L10n::$locales['AuthorMode: authors'] = 'AuthorMode : auteur·rice·s';
