49300a8d37f5448304ce3a7a43a2c560ee79de9a
franck <carnet.franck.paul@gmail.com>
2023-08-23 11:45:17 +0200

    New release 4.0

--- _define.php
+++ _define.php
@@ -13,7 +13,7 @@ $this->registerModule(
     'authorMode',
     'Post entries per author + author desc handling',
     'xave, Pierre Van Glabeke, Franck Paul',
-    '3.5',
+    '4.0',
     [
         'requires'    => [['core', '2.27'], ['php', '8.1']],
         'permissions' => dcCore::app()->auth->makePermissions([
