d877ddc720213d4100f5cae149382e511e4fe887
franck <carnet.franck.paul@gmail.com>
2024-09-16 16:05:24 +0200

    New release 5.5

--- _define.php
+++ _define.php
@@ -13,7 +13,7 @@ $this->registerModule(
     'Author Mode',
     'Post entries per author + author desc handling',
     'xave, Pierre Van Glabeke, Franck Paul',
-    '5.4',
+    '5.5',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
