647c859a2cd2a90431ca36626390dd0cf50f74cb
franck <contact@open-time.net>
2026-03-22 16:05:39 +0100

    New release 8.4.1

--- _define.php
+++ _define.php
@@ -14,9 +14,9 @@ $this->registerModule(
     'Author Mode',
     'Post entries per author + author desc handling',
     'xave, Pierre Van Glabeke, Franck Paul',
-    '8.4',
+    '8.4.1',
     [
-        'date'     => '2026-03-17T14:59:29+0100',
+        'date'     => '2026-03-22T16:05:39+0100',
         'requires' => [
             ['core', '2.36'],
             ['TemplateHelper'],
